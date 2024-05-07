7cc33745f5f93a0576807050a7a09acad797e24d
miracoly <68049792+miracoly@users.noreply.github.com>
2024-05-07 21:38:55 +0200

    feat(ch4): print lines with numbers

---
+++ ch3-4/lnums/app/Main.hs
@@ -0,0 +1,56 @@
+module Main (main) where
+
+import Data.Char (isPrint, isSeparator)
+import Data.Maybe
+import Lib (PadMode (..), numberAllLines, prettyNumberedLines)
+import System.Environment (getArgs, getProgName)
+
+main :: IO ()
+main = do
+  cliArgs <- getArgs
+  let (mFilePath, _) = parseArguments cliArgs
+  maybe
+    (printHelpText "Missing filename")
+    ( \filePath -> do
+        fileLines <- readLines filePath
+        let numbered = numberAllLines fileLines
+            prettyNumbered = prettyNumberedLines PadLeft numbered
+        mapM_ putStrLn prettyNumbered
+    )
+    mFilePath
+
+readLines :: FilePath -> IO [String]
+readLines filePath = do
+  contents <- readFile filePath
+  return (lines contents)
+
+data LineNumberOption
+  = ReverseNumbering
+  | SkipEmptyLines
+  | LeftAlign
+  deriving (Eq)
+
+parseArguments :: [String] -> (Maybe FilePath, [LineNumberOption])
+parseArguments args = case reverse args of
+  [] -> (Nothing, [])
+  (filename : options) ->
+    ( Just filename
+    , mapMaybe lnOptionFromString options
+    )
+
+lnOptionFromString :: String -> Maybe LineNumberOption
+lnOptionFromString "--reverse" = Just ReverseNumbering
+lnOptionFromString "--skip-empty" = Just SkipEmptyLines
+lnOptionFromString "--left-align" = Just LeftAlign
+lnOptionFromString _ = Nothing
+
+printHelpText :: String -> IO ()
+printHelpText msg = do
+  putStrLn (msg ++ "\n")
+  progName <- getProgName
+  putStrLn ("Usage: " ++ progName ++ " <options> <filename>")
+  putStrLn "\n"
+  putStrLn " Options:"
+  putStrLn "   --reverse      - Reverse the numbering"
+  putStrLn "   --skip-empty   - Skip numbering empty lines"
+  putStrLn "   --left-align   - Use left-aligned line numbers"
---
+++ ch3-4/lnums/src/Lib.hs
@@ -0,0 +1,66 @@
+module Lib (module Lib) where
+
+import Data.Char (isPrint, isSeparator)
+
+type NumberedLine = (Maybe Int, String)
+type NumberedLines = [NumberedLine]
+
+
+numberAllLines :: [String] -> NumberedLines
+numberAllLines = numberLines (const True) (const True)
+
+numberNonEmptyLines :: [String] -> NumberedLines
+numberNonEmptyLines = numberLines (not . isEmpty) (const True)
+
+numberAndIncrementNonEmptyLines :: [String] -> NumberedLines
+numberAndIncrementNonEmptyLines = numberLines (not . isEmpty) (not . isEmpty)
+
+numberLines :: (String -> Bool) -> (String -> Bool) -> [String] -> NumberedLines
+numberLines shouldIncr shouldNum lns =
+  let
+    go :: Int -> [String] -> NumberedLines
+    go _ [] = []
+    go counter (s : ss) =
+      let
+        mNumbering = if shouldNum s then Just counter else Nothing
+        newCounter = if shouldIncr s then counter + 1 else counter
+       in
+        (mNumbering, s) : go newCounter ss
+   in
+    go 1 lns
+
+isEmpty :: String -> Bool
+isEmpty str =
+  null str || all (\s -> not (isPrint s) || isSeparator s) str
+
+data PadMode = PadLeft | PadRight | PadCenter
+
+prettyNumberedLines :: PadMode -> NumberedLines -> [String]
+prettyNumberedLines mode lnsNums = 
+  let
+    (numbers, lns) = unzip lnsNums
+    numberStrs = map (maybe "" show) numbers
+    maxLen = maximum (map length numberStrs)
+    paddedNums = map (pad mode maxLen) numberStrs
+  in zipWith (\n l -> n ++ " " ++ l) paddedNums lns
+
+padLeft :: Int -> String -> String
+padLeft = pad PadLeft
+
+padRight :: Int -> String -> String
+padRight = pad PadRight
+
+padCenter :: Int -> String -> String
+padCenter = pad PadCenter
+
+pad :: PadMode -> Int -> String -> String
+pad padMode desiredLen str =
+  let
+    diff = desiredLen - length str
+    padding = replicate diff ' '
+   in
+    case padMode of
+      PadLeft -> padding <> str
+      PadRight -> str <> padding
+      PadCenter ->
+        let halfPad = replicate (div diff 2) ' ' in halfPad <> str <> halfPad
