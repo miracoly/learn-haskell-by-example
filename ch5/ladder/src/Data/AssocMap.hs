41ca6b541156cec9e1dc71d568282fcb232edb74
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-16 13:13:34 +0200

    feat(ch5): extend AssocMap

--- ch5/ladder/src/Data/AssocMap.hs
+++ ch5/ladder/src/Data/AssocMap.hs
@@ -4,12 +4,18 @@ module Data.AssocMap
     delete,
     insert,
     member,
+    lookup,
+    findWithDefault
   )
 where
 
 import Graph qualified as G (alter, member)
+import Prelude hiding (lookup)
+import Prelude qualified as P
+import Data.Maybe (fromMaybe)
 
 newtype AssocMap k v = AssocMap [(k, v)]
+  deriving (Show, Eq)
 
 empty :: AssocMap k v
 empty = AssocMap []
@@ -20,9 +26,14 @@ delete = alter (const Nothing)
 insert :: (Eq k) => k -> v -> AssocMap k v -> AssocMap k v
 insert key value = alter (const (Just value)) key
 
--- | Add a node to the Associative Map
 member :: (Eq k) => k -> AssocMap k v -> Bool
 member key (AssocMap am) = G.member key am
+
+lookup :: (Eq k) => k -> AssocMap k v -> Maybe v
+lookup key (AssocMap am) = P.lookup key am
+
+findWithDefault :: (Eq k) => v -> k -> AssocMap k v -> v
+findWithDefault def key = fromMaybe def . lookup key
 
 alter :: (Eq k) => (Maybe v -> Maybe v) -> k -> AssocMap k v -> AssocMap k v
 alter f key (AssocMap am) = AssocMap $ G.alter f key am
