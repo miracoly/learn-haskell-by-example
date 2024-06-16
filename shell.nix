dafb30df1f7364cbba52a03c385cf20c376cffcc
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-16 11:34:35 +0200

    build: add hoogle

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 let
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cc54fb41d13736e92229c21627ea4f22199fee6b.tar.gz") {};
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cc54fb41d13736e92229c21627ea4f22199fee6b.tar.gz") { };
   ghcVersion = "ghc965";
   ghcWithPackages = pkgs.haskell.packages.${ghcVersion}.ghcWithPackages (pkgs: with pkgs; [
     haskell-language-server
+    hoogle
   ]);
 in
 pkgs.mkShell {
