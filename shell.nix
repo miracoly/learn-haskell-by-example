70f3990c07dc1f69ffa22e5c7ae73a16afd2b677
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-16 09:22:49 +0200

    refactor: pin nix & ghc version

--- shell.nix
+++ shell.nix
@@ -1,6 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cc54fb41d13736e92229c21627ea4f22199fee6b.tar.gz") {};
+  ghcVersion = "ghc965";
+  ghcWithPackages = pkgs.haskell.packages.${ghcVersion}.ghcWithPackages (pkgs: with pkgs; [
+    haskell-language-server
+  ]);
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    ghcWithPackages
     stack
     zlib.dev
   ];
