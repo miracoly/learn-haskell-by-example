c00dab002f98fb01aad3b41236ae54c219adcb6c
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-19 16:40:40 +0200

    feat(ch5): add function to Graph module

--- ch5/ladder/src/Data/AssocMap.hs
+++ ch5/ladder/src/Data/AssocMap.hs
@@ -4,6 +4,7 @@ module Data.AssocMap
     delete,
     insert,
     member,
+    alter,
     lookup,
     findWithDefault
   )
--- ch5/ladder/src/Graph.hs
+++ ch5/ladder/src/Graph.hs
@@ -1,1 +1,21 @@
 module Graph (module Graph) where
+
+import Data.AssocMap qualified as AM
+import Data.List qualified as L
+
+type DiGraph a = AM.AssocMap a [a]
+
+addEdges :: (Eq a) => [(a, a)] -> DiGraph a -> DiGraph a
+addEdges edges graph = L.foldr addEdge graph edges
+
+addEdge :: (Eq a) => (a, a) -> DiGraph a -> DiGraph a
+addEdge (node, child) = AM.alter insertEdge node
+  where
+    insertEdge Nothing = Just [child]
+    insertEdge (Just nodes) = Just (L.nub (child : nodes))
+
+buildDiGraph :: (Eq a) => [(a, [a])] -> DiGraph a
+buildDiGraph = L.foldr (uncurry AM.insert) AM.empty
+
+children :: (Eq a) => a -> DiGraph a -> [a]
+children = AM.findWithDefault []
