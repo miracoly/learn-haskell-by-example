d0024baa8ee2a7867e08786083ca6a64962bde47
miracoly <68049792+miracoly@users.noreply.github.com>
2024-06-19 15:53:08 +0200

    refactor(ch5): move helper functions into AssocMap module

--- ch5/ladder/src/Data/AssocMap.hs
+++ ch5/ladder/src/Data/AssocMap.hs
@@ -9,10 +9,9 @@ module Data.AssocMap
   )
 where
 
-import Graph qualified as G (alter, member)
 import Prelude hiding (lookup)
 import Prelude qualified as P
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, isJust)
 
 newtype AssocMap k v = AssocMap [(k, v)]
   deriving (Show, Eq)
@@ -27,7 +26,7 @@ insert :: (Eq k) => k -> v -> AssocMap k v -> AssocMap k v
 insert key value = alter (const (Just value)) key
 
 member :: (Eq k) => k -> AssocMap k v -> Bool
-member key (AssocMap am) = G.member key am
+member key (AssocMap am) = _member key am
 
 lookup :: (Eq k) => k -> AssocMap k v -> Maybe v
 lookup key (AssocMap am) = P.lookup key am
@@ -36,4 +35,33 @@ findWithDefault :: (Eq k) => v -> k -> AssocMap k v -> v
 findWithDefault def key = fromMaybe def . lookup key
 
 alter :: (Eq k) => (Maybe v -> Maybe v) -> k -> AssocMap k v -> AssocMap k v
-alter f key (AssocMap am) = AssocMap $ G.alter f key am
+alter f key (AssocMap am) = AssocMap $ _alter f key am
+
+type Graph k v = [(k, v)]
+type DiGraph a = [(a, [a])]
+
+-- | Add a node to the graph
+_member :: (Eq k) => k -> Graph k v -> Bool
+_member = (isJust .) . P.lookup
+
+-- | Like member, but with arguments flipped
+_hasNode :: (Eq k) => Graph k v -> k -> Bool
+_hasNode = flip _member
+
+-- | Add a node with empty adjacency list to the graph
+_addNode :: (Eq a) => DiGraph a -> a -> DiGraph a
+_addNode graph node
+  | graph `_hasNode` node = graph
+  | otherwise = (node, []) : graph
+
+_alter :: (Eq k) => (Maybe v -> Maybe v) -> k -> Graph k v -> Graph k v
+_alter f key [] =
+  case f Nothing of
+    Nothing -> []
+    Just value -> [(key, value)]
+_alter f key ((k, v) : kvs)
+  | key == k =
+      case f (Just v) of
+        Nothing -> kvs
+        Just value -> (key, value) : kvs
+  | otherwise = (k, v) : _alter f key kvs
