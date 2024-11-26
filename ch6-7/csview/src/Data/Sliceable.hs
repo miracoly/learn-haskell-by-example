fb66cfa91ebd8000c0d2fe211ef4e06364f4a3b5
miracoly <68049792+miracoly@users.noreply.github.com>
2024-11-26 08:29:52 +0100

    feat(ch6): add Slicable instances for List and Maybe

--- ch6-7/csview/src/Data/Sliceable.hs
+++ ch6-7/csview/src/Data/Sliceable.hs
@@ -4,3 +4,18 @@ class Sliceable a where
   slice :: Int -> Int -> a -> a
   slice idx1 idx2 xs = let (_, s, _) = slicePartition idx1 idx2 xs in s
   slicePartition :: Int -> Int -> a -> (a, a, a)
+
+instance Sliceable [a] where
+  slicePartition idx1 idx2 xs =
+    ( take idx1 xs,
+      take (idx2 - idx1) $ drop idx1 xs,
+      drop idx2 xs
+    )
+
+instance (Sliceable a) => Sliceable (Maybe a) where
+  slicePartition idx1 idx2 m =
+    case m of
+      Nothing -> (Nothing, Nothing, Nothing)
+      Just xs ->
+        let (hd, s, tl) = slicePartition idx1 idx2 xs
+         in (Just hd, Just s, Just tl)
