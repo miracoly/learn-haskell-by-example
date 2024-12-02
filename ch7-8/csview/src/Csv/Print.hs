881a4bf3f59985dbe0992c3ce60abaf1fbc458f1
miracoly <68049792+miracoly@users.noreply.github.com>
2024-12-02 08:16:00 +0100

    feat(ch8): introduce Csv.Print module

---
+++ ch7-8/csview/src/Csv/Print.hs
@@ -0,0 +1,23 @@
+module Csv.Print (module Csv.Print) where
+
+import Csv (dataFieldToText)
+import Csv.Types
+import qualified Data.List as L
+import qualified Data.Maybe as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+data PrettyCsv = PrettyCsv
+  { pcHeader :: Maybe [T.Text],
+    pcColumns :: [[T.Text]],
+    pcSummaries :: Maybe [T.Text]
+  }
+  deriving (Eq, Show)
+
+writeCsv :: FilePath -> Csv -> IO ()
+writeCsv path = TIO.writeFile path . T.intercalate "\n" . toFileContent
+
+toFileContent :: Csv -> [T.Text]
+toFileContent Csv {..} =
+  let rows = L.map (L.map dataFieldToText) $ L.transpose csvColumns
+   in L.map (T.intercalate ",") $ M.maybe rows (: rows) csvHeader
