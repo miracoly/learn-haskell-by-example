916e7a7797c80a04a0e1f68b4422a5ab22eb6b54
miracoly <68049792+miracoly@users.noreply.github.com>
2024-12-02 09:14:28 +0100

    feat(ch8): add prettyText
    feat(ch8): add prettyText - Wip
    
    squash
    
    wip
    
    wip

--- ch7-8/csview/src/Csv.hs
+++ ch7-8/csview/src/Csv.hs
@@ -1,8 +1,20 @@
 module Csv
   ( module Csv.Types,
     module Csv.Parsing,
+    module Csv.Print,
+    printCsv,
   )
 where
 
 import Csv.Parsing
+import Csv.Print
 import Csv.Types
+import qualified Data.Either as E
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+printCsv :: FilePath -> IO ()
+printCsv fp = do
+  file <- readFile fp
+  let unsafePrettify = prettyText . fromCsv . E.fromRight (error "bla") . parseWithHeader . T.pack
+  TIO.putStrLn $ unsafePrettify file
--- ch7-8/csview/src/Csv/Print.hs
+++ ch7-8/csview/src/Csv/Print.hs
@@ -1,6 +1,6 @@
 module Csv.Print (module Csv.Print) where
 
-import Csv (dataFieldToText)
+import Csv.Parsing
 import Csv.Types
 import qualified Data.Either as E
 import qualified Data.List as L
@@ -31,6 +31,23 @@ withSummaries csv@(PrettyCsv {..}) summaries =
   if L.length pcColumns == L.length summaries
     then Right csv {pcSummaries = Just summaries}
     else Left "Summary must have same column length as Csv"
+
+pretty :: PrettyCsv -> String
+pretty = T.unpack . prettyText
+
+prettyText :: PrettyCsv -> T.Text
+prettyText PrettyCsv {..} =
+  let paddedCols = map padCol pcColumns
+      rows = L.transpose paddedCols
+      concatedRows = map (L.foldr1 (\a c -> a <> "  " <> c)) rows
+   in T.intercalate "\n" concatedRows
+  where
+    maxColWidth :: [T.Text] -> Int
+    maxColWidth = maximum . map T.length
+    padCol :: [T.Text] -> [T.Text]
+    padCol col = fmap (padField (maxColWidth col)) col
+    padField :: Int -> T.Text -> T.Text
+    padField max' txt = T.replicate (max' - T.length txt) " " <> txt
 
 writeCsv :: FilePath -> Csv -> IO ()
 writeCsv path = TIO.writeFile path . T.intercalate "\n" . toFileContent
