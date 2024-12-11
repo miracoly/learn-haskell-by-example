433ed80c1975772e3f6904ef797468c726ddaca5
miracoly <68049792+miracoly@users.noreply.github.com>
2024-12-11 09:23:26 +0100

    wip

--- ch7-8/csview/src/Util/Arguments.hs
+++ ch7-8/csview/src/Util/Arguments.hs
@@ -3,21 +3,27 @@ module Util.Arguments (module Util.Arguments) where
 import qualified Data.List as L
 import qualified Data.Text as T
 import System.Environment (getArgs)
-import qualified Data.Maybe as M
 
 getArguments :: IO [T.Text]
 getArguments = fmap (map T.pack) getArgs
 
 getValueOf :: T.Text -> IO (Maybe T.Text)
-getValueOf key = undefined -- TODO
-  where
-    argKey = "--" <> key <> "="
+getValueOf key =
+  let argKey = "--" <> key <> "="
+   in do
+        args <- getArguments
+        return $ L.find (T.isPrefixOf argKey) args >>= T.stripPrefix argKey
 
 getBool :: T.Text -> IO Bool
 getBool key = let argKey = "--" <> key in L.elem argKey <$> getArguments
 
 getChar :: T.Text -> IO (Maybe Char)
-getChar = undefined
+getChar key = do
+  mArg <- getValueOf key
+  return $  mArg >>= T.uncons >>= fstIfOne
+  where
+    fstIfOne :: (Char, T.Text) -> Maybe Char
+    fstIfOne (c, rest) = if T.null rest then Just c else Nothing
 
 getText :: T.Text -> IO (Maybe T.Text)
 getText = undefined
