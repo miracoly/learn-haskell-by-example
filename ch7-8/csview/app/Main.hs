3b2419e2268ed1a0e965afe6822e82be3057109d
miracoly <68049792+miracoly@users.noreply.github.com>
2024-12-29 17:51:53 +0100

    feat(ch8): implement parseInFile

--- ch7-8/csview/app/Main.hs
+++ ch7-8/csview/app/Main.hs
@@ -1,7 +1,37 @@
 module Main (main) where
 
-import Csv (printCsv)
+import qualified Csv
+import qualified Data.Maybe as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Util.Arguments as Args
+
+parseInFile :: T.Text -> IO (Either String Csv.Csv)
+parseInFile key = do
+  mInFile <- Args.getText key
+  mFieldSep <- Args.getChar "field-separator"
+  hasHeader <- Args.getBool "with-header"
+  let separators =
+        Csv.defaultSeparators
+          { Csv.sepFieldSeparator =
+              M.fromMaybe
+                (Csv.sepFieldSeparator Csv.defaultSeparators)
+                mFieldSep
+          }
+      headerOpt =
+        if hasHeader
+          then Csv.WithHeader
+          else Csv.WithoutHeader
+      parseOpts =
+        Csv.CsvParseOptions
+          { Csv.cpoSeparators = separators
+          , Csv.cpoHeaderOptions = headerOpt
+          }
+  case mInFile of
+    Just inFile -> do
+      contents <- TIO.readFile $ T.unpack inFile
+      return $ Csv.parseCsv parseOpts contents
+    _ -> return $ Left "argument not set"
 
 main :: IO ()
-main = printCsv "csvs/cities.csv"
-
+main = Csv.printCsv "csvs/cities.csv"
